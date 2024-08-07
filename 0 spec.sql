3774e27f76cb896bd4ea052f7302d0d72a44ca12
yisraelkramer <yisraelkramer@gmail.com>
2024-08-06 21:46:48 -0400

    spec

---
+++ 0 spec.sql
@@ -0,0 +1,64 @@
+/*
+At Strike Zone Bowling Alley, we provide a fun and exciting experience for bowlers of all ages through lane rentals. 
+To manage our operations efficiently, comply with regulations, and understand who our main clientele is, 
+we need to keep detailed electronic records of all lane reservations. 
+This includes customer details, reservation times, lane numbers, and customer age.
+
+Question: Do you need to record the end time for lane reservations?
+Answer: Yes, we need both the start time and end time for each lane reservation.
+There is also a minimum amount of 30 minutes required.
+
+Question: How many lanes do you have in your bowling alley?
+Answer: We have 20 lanes available for reservations.
+
+Question: What hours are you open? 
+Answer: We are open Sunday through Friday from 9 - 11 
+
+Reports: 
+1) I want to find out our busiest day of the week, 
+the busiest time of the day that people start playing
+and the average time that people spend there.
+use as many statements as necessary
+
+2) We would like a list of how many per age group. 
+Age group brackets: under 18 , 18-30, between 30- 45, and 45+ 
+The group with the most should go on top.
+
+3) All people ages 18 through 30 are joining the league that we are
+starting and we need a list of them preseted as follows:
+Last name, first name (age) 
+
+Sample data:
+Miriam Cohen, 16, 3, 2024-08-04, 15:00, 16:30
+Jacob Goldstein, 26, 7, 2024-08-09, 15:30, 17:00
+Rachel Schwartz, 15, 14, 2024-08-06, 21:00, 22:30
+Esther Rosen, 19, 9, 2024-08-06, 09:30, 11:00
+David Levine, 45, 6, 2024-08-05, 11:00, 13:00
+Aaron Weiss, 17, 15, 2024-08-07, 09:00, 11:00
+Hannah Friedman, 27, 18, 2024-08-07, 15:30, 17:00
+Shoshana Katz, 24, 1, 2024-08-08, 09:00, 10:30
+Leah Greenberg, 40, 19, 2024-08-07, 17:30, 19:00
+Samuel Adler, 32, 16, 2024-08-07, 11:30, 13:00
+Moses Cohen, 31, 8, 2024-08-09, 17:30, 19:00
+Abraham Katz, 42, 10, 2024-08-06, 12:00, 13:30
+Rebecca Stein, 45, 10, 2024-08-09, 21:00, 22:30
+Isaac Levine, 29, 12, 2024-08-06, 17:00, 19:00
+Sarah Rosen, 38, 11, 2024-08-06, 14:30, 16:00
+Sophie Goldberg, 37, 3, 2024-08-08, 13:00, 14:30
+Miriam Klein, 14, 9, 2024-08-09, 19:30, 21:00
+Jacob Miller, 32, 7, 2024-08-05, 14:00, 16:00
+Eli Green, 23, 9, 2024-08-05, 20:00, 21:30
+Tamar Gold, 25, 1, 2024-08-04, 10:00, 11:30
+Solomon Rosen, 33, 2, 2024-08-08, 11:00, 12:30
+Yitzhak Weiss, 41, 6, 2024-08-09, 13:00, 15:00
+Noah Katz, 35, 20, 2024-08-07, 19:30, 21:00
+Chaya Greenberg, 27, 8, 2024-08-05, 17:00, 19:30
+Shlomo Weiss, 32, 16, 2024-08-07, 11:30, 13:00
+Hannah Cohen, 27, 18, 2024-08-07, 15:30, 17:00
+Isaac Goldberg, 26, 7, 2024-08-09, 15:30, 17:00
+Shoshana Stein, 24, 1, 2024-08-08, 09:00, 10:30
+Daniel Green, 23, 9, 2024-08-05, 20:00, 21:30
+Rachel Weiss, 15, 14, 2024-08-06, 21:00, 22:30
+Leah Miller, 40, 19, 2024-08-07, 17:30, 19:00
+David Gold, 22, 10, 2024-08-09, 09:00, 10:30
+*/
